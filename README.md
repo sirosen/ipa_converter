dbf15cdc3a8d88bb88bcc9f118db0650690e2fa9
Stephen Rosen <sirosen@uchicago.edu>
2013-01-12 10:58:33 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ipa_sampa
 =========
 
 A simple python tool to convert IPA notation to SAMPA.
+
+Robustness
+=========
+This tool IS NOT robust.
+It was built on a single linux system without careful consideration for issues with encoding.
+I would greatly appreciate any efforts to make a version of this which is platform independent.
+
+I believe it may serve as a useful starting point for others tying to convert between IPA and SAMPA.
---
+++ convert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from string import printable
+import sys
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Converts IPA input to SAMPA output. Does not handle certain cases: nasals, tones, syllabic consonants, and non-SAMPA representable phonetic symbols. Writes to stdout.')
+    parser.add_argument('-c','--config',help='Name of the configuration table. Defaults to "table.cfg" in the working directory.',default='table.cfg')
+    parser.add_argument('source',help='IPA symbols to convert to SAMPA.')
+    args = parser.parse_args(sys.argv[1:])
+    config = args.config
+    source = args.source.decode('utf-8')
+
+    config = open(config,'r')
+    table = {}
+    for line in config:
+        line=line.strip()
+        if line == '': continue
+        row = line.split()
+        sampa_symb = row[0].decode('utf-8')
+        ipa_symb = row[1].decode('utf-8')
+        table[ipa_symb] = sampa_symb
+
+    out = []
+    for c in source:
+        if c in table: c = table[c]
+        if c not in printable: c = ''
+        out.append(c)
+    print(''.join(out).encode('utf-8'))
